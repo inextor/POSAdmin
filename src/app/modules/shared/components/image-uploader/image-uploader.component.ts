74a6a7e9f6bbdf6616dd2cfbba7ea4128542a8c2
nextor <nextor@gmail.com>
2023-11-18 12:17:06 -0800

    Agregnaod moduleos

---
+++ src/app/modules/shared/components/image-uploader/image-uploader.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-image-uploader',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './image-uploader.component.html',
+  styleUrl: './image-uploader.component.css'
+})
+export class ImageUploaderComponent {
+
+}
